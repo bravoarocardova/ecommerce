cb66caf6d15657987fb117e1a0aff775bf374fc9
Bravo Aro Cardova <bravoarocardova@gmail.com>
2023-05-02 20:27:42 +0700

    fix dashboard admin

--- app/Controllers/Admin/Admin.php
+++ app/Controllers/Admin/Admin.php
@@ -3,15 +3,97 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
-use App\Models\BarangServisM;
+use App\Models\PembelianM;
 use App\Models\DataServisM;
-use App\Models\JasaServisM;
+use App\Models\PembelianProdukM;
 
 class Admin extends BaseController
 {
 
+  private $pembelianM;
+  private $dataServisM;
+  private $pembelianProdukM;
+
+  public function __construct()
+  {
+    $this->pembelianM = new PembelianM();
+    $this->dataServisM  = new DataServisM();
+    $this->pembelianProdukM = new PembelianProdukM();
+  }
+
   public function dashboard()
   {
-    return view('admin/dashboard');
+
+    $getchartpendapatan = $this->pembelianM->select('YEAR(pembelian.`updated_at`) as year, MONTH(pembelian.`updated_at`) as month, SUM(total_pembelian) as pendapatan')
+      ->where("YEAR(pembelian.`updated_at`) = '" . date('Y') . "' AND `status_pembelian` = 'Selesai'")
+      ->groupBy('YEAR(pembelian.`updated_at`), MONTH(pembelian.`updated_at`)')
+      ->orderBy('1,2')->find();
+
+    $getchartterjual = $this->pembelianProdukM->select('YEAR(pembelian.`updated_at`) as year, MONTH(pembelian.`updated_at`) as month, SUM(jumlah) as terjual ')
+      ->join('pembelian', 'pembelian_produk.id_pembelian = pembelian.id_pembelian')
+      ->where("YEAR(pembelian_produk.`updated_at`) = '" . date('Y') . "' AND `status_pembelian` = 'Selesai'")
+      ->groupBy('YEAR(pembelian.`updated_at`), MONTH(pembelian.`updated_at`)')
+      ->orderBy('1,2')->find();
+
+
+    $chartPendapatan = null;
+    $chartTerjual = null;
+
+    for ($i = 1; $i <= 12; $i++) {
+
+      foreach ($getchartpendapatan as $g) {
+        if ($g['month'] == $i) {
+          $chartPendapatan[] = $g['pendapatan'];
+        } else {
+          $chartPendapatan[] = 0;
+        }
+      }
+
+      foreach ($getchartterjual as $g) {
+        if ($g['month'] == $i) {
+          $chartTerjual[] = $g['terjual'];
+        } else {
+          $chartTerjual[] = 0;
+        }
+      }
+    }
+
+    return view(
+      'admin/dashboard',
+      [
+        'pendapatan_penjualan' =>  $this->pembelianM->selectSum('total_pembelian', 'pendapatan')
+          ->where(
+            [
+              'status_pembelian' => 'Selesai',
+              'MONTH(updated_at)' => date('m'),
+              'YEAR(updated_at)' => date('Y')
+            ]
+          )->first(),
+
+        'penjualan_produk' => $this->pembelianProdukM->selectSum('jumlah')
+          ->join('pembelian', 'pembelian_produk.id_pembelian = pembelian.id_pembelian')
+          ->where(
+            [
+              'pembelian.status_pembelian' => 'Selesai',
+              'MONTH(pembelian.updated_at)' => date('m'),
+              'YEAR(pembelian.updated_at)' => date('Y')
+            ]
+          )->first(),
+
+        'servis_masuk' => $this->dataServisM->select()
+          ->where("status IS NULL OR `status` NOT IN ('selesai','dibatalkan')")->countAllResults(),
+
+        'pendapatan_servis' => $this->dataServisM->selectSum('total_biaya')
+          ->where(
+            [
+              'status' => 'selesai',
+              'MONTH(updated_at)' => date('m'),
+              'YEAR(updated_at)' => date('Y')
+            ]
+          )->first(),
+        'chart_pendapatan' => $chartPendapatan,
+        'chart_terjual' => $chartTerjual,
+      ]
+    );
   }
 }
--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -15,7 +15,7 @@
               <div class="card-body">
                 <div class="row">
                   <div class="col mt-0">
-                    <h5 class="card-title">Sales</h5>
+                    <h5 class="card-title">Penjualan Produk</h5>
                   </div>
 
                   <div class="col-auto">
@@ -24,10 +24,10 @@
                     </div>
                   </div>
                 </div>
-                <h1 class="mt-1 mb-3">2.382</h1>
+                <h1 class="mt-1 mb-3"><?= $penjualan_produk['jumlah'] ?? 0 ?></h1>
                 <div class="mb-0">
-                  <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
-                  <span class="text-muted">Since last week</span>
+                  <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i></span>
+                  <span class="text-muted"></span>
                 </div>
               </div>
             </div>
@@ -35,19 +35,19 @@
               <div class="card-body">
                 <div class="row">
                   <div class="col mt-0">
-                    <h5 class="card-title">Visitors</h5>
+                    <h5 class="card-title">Servis Masuk</h5>
                   </div>
 
                   <div class="col-auto">
                     <div class="stat text-primary">
-                      <i class="align-middle" data-feather="users"></i>
+                      <i class="align-middle" data-feather="sliders"></i>
                     </div>
                   </div>
                 </div>
-                <h1 class="mt-1 mb-3">14.212</h1>
+                <h1 class="mt-1 mb-3"><?= $servis_masuk ?></h1>
                 <div class="mb-0">
-                  <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.25% </span>
-                  <span class="text-muted">Since last week</span>
+                  <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i></span>
+                  <span class="text-muted"></span>
                 </div>
               </div>
             </div>
@@ -57,7 +57,7 @@
               <div class="card-body">
                 <div class="row">
                   <div class="col mt-0">
-                    <h5 class="card-title">Earnings</h5>
+                    <h5 class="card-title">Pendapatan Penjualan</h5>
                   </div>
 
                   <div class="col-auto">
@@ -66,10 +66,10 @@
                     </div>
                   </div>
                 </div>
-                <h1 class="mt-1 mb-3">$21.300</h1>
+                <h1 class="mt-1 mb-3">Rp. <?= number_format($pendapatan_penjualan['pendapatan']) ?? 0 ?></h1>
                 <div class="mb-0">
-                  <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 6.65% </span>
-                  <span class="text-muted">Since last week</span>
+                  <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i></span>
+                  <span class="text-muted"></span>
                 </div>
               </div>
             </div>
@@ -77,19 +77,19 @@
               <div class="card-body">
                 <div class="row">
                   <div class="col mt-0">
-                    <h5 class="card-title">Orders</h5>
+                    <h5 class="card-title">Pendapatan Servis</h5>
                   </div>
 
                   <div class="col-auto">
                     <div class="stat text-primary">
-                      <i class="align-middle" data-feather="shopping-cart"></i>
+                      <i class="align-middle" data-feather="dollar-sign"></i>
                     </div>
                   </div>
                 </div>
-                <h1 class="mt-1 mb-3">64</h1>
+                <h1 class="mt-1 mb-3">Rp. <?= number_format($pendapatan_servis['total_biaya']) ?></h1>
                 <div class="mb-0">
-                  <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.25% </span>
-                  <span class="text-muted">Since last week</span>
+                  <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i></span>
+                  <span class="text-muted"></span>
                 </div>
               </div>
             </div>
@@ -118,47 +118,16 @@
       <div class="card flex-fill w-100">
         <div class="card-header">
 
-          <h5 class="card-title mb-0">Browser Usage</h5>
+          <h5 class="card-title mb-0">Monthly Sales</h5>
         </div>
-        <div class="card-body d-flex">
-          <div class="align-self-center w-100">
-            <div class="py-3">
-              <div class="chart chart-xs">
-                <canvas id="chartjs-dashboard-pie"></canvas>
-              </div>
-            </div>
-
-            <table class="table mb-0">
-              <tbody>
-                <tr>
-                  <td>Chrome</td>
-                  <td class="text-end">4306</td>
-                </tr>
-                <tr>
-                  <td>Firefox</td>
-                  <td class="text-end">3801</td>
-                </tr>
-                <tr>
-                  <td>IE</td>
-                  <td class="text-end">1689</td>
-                </tr>
-              </tbody>
-            </table>
+        <div class="card-body d-flex w-100">
+          <div class="align-self-center chart chart-lg">
+            <canvas id="chartjs-dashboard-bar"></canvas>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-12 col-md-12 col-xxl-6 d-flex order-3 order-xxl-2">
-      <div class="card flex-fill w-100">
-        <div class="card-header">
 
-          <h5 class="card-title mb-0">Real-Time</h5>
-        </div>
-        <div class="card-body px-4">
-          <div id="world_map" style="height:350px;"></div>
-        </div>
-      </div>
-    </div>
     <div class="col-12 col-md-6 col-xxl-3 d-flex order-1 order-xxl-1">
       <div class="card flex-fill">
         <div class="card-header">
@@ -176,99 +145,134 @@
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-12 col-lg-8 col-xxl-9 d-flex">
-      <div class="card flex-fill">
-        <div class="card-header">
-
-          <h5 class="card-title mb-0">Latest Projects</h5>
-        </div>
-        <table class="table table-hover my-0">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th class="d-none d-xl-table-cell">Start Date</th>
-              <th class="d-none d-xl-table-cell">End Date</th>
-              <th>Status</th>
-              <th class="d-none d-md-table-cell">Assignee</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Project Apollo</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-success">Done</span></td>
-              <td class="d-none d-md-table-cell">Vanessa Tucker</td>
-            </tr>
-            <tr>
-              <td>Project Fireball</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-danger">Cancelled</span></td>
-              <td class="d-none d-md-table-cell">William Harris</td>
-            </tr>
-            <tr>
-              <td>Project Hades</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-success">Done</span></td>
-              <td class="d-none d-md-table-cell">Sharon Lessman</td>
-            </tr>
-            <tr>
-              <td>Project Nitro</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-warning">In progress</span></td>
-              <td class="d-none d-md-table-cell">Vanessa Tucker</td>
-            </tr>
-            <tr>
-              <td>Project Phoenix</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-success">Done</span></td>
-              <td class="d-none d-md-table-cell">William Harris</td>
-            </tr>
-            <tr>
-              <td>Project X</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-success">Done</span></td>
-              <td class="d-none d-md-table-cell">Sharon Lessman</td>
-            </tr>
-            <tr>
-              <td>Project Romeo</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-success">Done</span></td>
-              <td class="d-none d-md-table-cell">Christina Mason</td>
-            </tr>
-            <tr>
-              <td>Project Wombat</td>
-              <td class="d-none d-xl-table-cell">01/01/2021</td>
-              <td class="d-none d-xl-table-cell">31/06/2021</td>
-              <td><span class="badge bg-warning">In progress</span></td>
-              <td class="d-none d-md-table-cell">William Harris</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <div class="col-12 col-lg-4 col-xxl-3 d-flex">
-      <div class="card flex-fill w-100">
-        <div class="card-header">
-
-          <h5 class="card-title mb-0">Monthly Sales</h5>
-        </div>
-        <div class="card-body d-flex w-100">
-          <div class="align-self-center chart chart-lg">
-            <canvas id="chartjs-dashboard-bar"></canvas>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
 </div>
 
+<?= $this->endSection() ?>
+
+<?= $this->section('script') ?>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    var cvs = document.getElementById("chartjs-dashboard-line");
+    if (cvs != null) {
+      var ctx = cvs.getContext("2d");
+      var gradient = ctx.createLinearGradient(0, 0, 0, 225);
+      gradient.addColorStop(0, "rgba(215, 227, 244, 1)");
+      gradient.addColorStop(1, "rgba(215, 227, 244, 0)");
+      // Line chart
+      new Chart(document.getElementById("chartjs-dashboard-line"), {
+        type: "line",
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          datasets: [{
+            label: "Sales (Rp.)",
+            fill: true,
+            backgroundColor: gradient,
+            borderColor: window.theme.primary,
+            data: <?= json_encode($chart_pendapatan) ?>
+          }]
+        },
+        options: {
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+          tooltips: {
+            intersect: false
+          },
+          hover: {
+            intersect: true
+          },
+          plugins: {
+            filler: {
+              propagate: false
+            }
+          },
+          scales: {
+            xAxes: [{
+              reverse: true,
+              gridLines: {
+                color: "rgba(0,0,0,0.0)"
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                stepSize: 1000
+              },
+              display: true,
+              borderDash: [3, 3],
+              gridLines: {
+                color: "rgba(0,0,0,0.0)"
+              }
+            }]
+          }
+        }
+      });
+    }
+  });
+</script>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    // Bar chart
+    var ctx = document.getElementById("chartjs-dashboard-bar");
+    if (ctx != null) {
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          datasets: [{
+            label: "This year",
+            backgroundColor: window.theme.primary,
+            borderColor: window.theme.primary,
+            hoverBackgroundColor: window.theme.primary,
+            hoverBorderColor: window.theme.primary,
+            data: <?= json_encode($chart_terjual) ?>,
+            barPercentage: .75,
+            categoryPercentage: .5
+          }]
+        },
+        options: {
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [{
+              gridLines: {
+                display: false
+              },
+              stacked: false,
+              ticks: {
+                stepSize: 20
+              }
+            }],
+            xAxes: [{
+              stacked: false,
+              gridLines: {
+                color: "transparent"
+              }
+            }]
+          }
+        }
+      });
+    }
+  });
+</script>
+
+<!-- Calender -->
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    var date = new Date(Date.now() - 0 * 24 * 60 * 60 * 1000);
+    var defaultDate = date.getUTCFullYear() + "-" + (date.getUTCMonth() + 1) + "-" + date.getUTCDate();
+    var calenderdiv = document.getElementById("datetimepicker-dashboard");
+    if (calenderdiv != null) {
+      calenderdiv.flatpickr({
+        inline: true,
+        prevArrow: "<span title=\"Previous month\">&laquo;</span>",
+        nextArrow: "<span title=\"Next month\">&raquo;</span>",
+        defaultDate: defaultDate
+      });
+    }
+  });
+</script>
 <?= $this->endSection() ?>
